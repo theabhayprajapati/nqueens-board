66c2562cfe37bc208fe7e0ac53bd35d97070874a
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2022-08-17 20:44:45 +0530

    now it is offline compaitible

--- sw.js
+++ sw.js
@@ -12,31 +12,36 @@ self.addEventListener('activate', function (event) {
 );
 
 // fetch event pwa
-self.addEventListener('fetch', function (event) {
-    console.log('Service Worker: Fetching')
-    console.log(event);
-    event.respondWith(fetch(event.request));
-}
-)
+
 // cache index.html file for offline use
 var CACHE_NAME = 'cache-v1';
 var urlsToCache = [
     '/',
     '/index.html',
-    '/css/style.css',
-    '/js/app.js',
-    '/js/db.js',
+    '/assets/favicon.ico',
+    '/assets/maze.png',
+
 ]
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
                 console.log('Opened cache');
-                var msg = document.getElementById('winning-message')
-                msg.innerHTML = 'Caching files...'
                 return cache.addAll(urlsToCache);
             })
     );
 }
 
 );
+self.addEventListener('fetch', function (event) {
+    console.log('Service Worker: Fetching')
+    console.log(event);
+    // show cache  data if available
+    event.respondWith(
+        caches.match(event.request).then(cache => {
+            return cache || fetch(event.request);
+        })
+    );
+
+}
+)
