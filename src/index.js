0341b7940d5909bd329a0e4cb0262147a1cfcb27
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2022-08-20 14:54:16 +0530

    offline working test successful

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { makeBlackWhite, resetAll, turnAllBoxesAsRed, updateColorsOfBoxes, vibrate, winningMessage } from './utils/utils.js';
+import { resetAll, turnAllBoxesAsRed, updateColorsOfBoxes, vibrate, winningMessage } from './utils/utils.js';
 var board = document.getElementById("board");
-const RED_SHADE = '#EA3C52'
 var boxes = [];
 var rows = [];
 var cols = [];
@@ -10,15 +9,10 @@ var total_rows = 8;
 var selected_location = [];
 var selected_places = [];
 var total_boxes = total_cols * total_rows;
-// make a 2d array
+
 for (var i = 0; i < total_rows; i++) {
-    matrix[i] = [];
+    console.log('starting to create maze.');
     for (var j = 0; j < total_cols; j++) {
-        matrix[i][j] = j;
-    }
-}
-for (var i = 0; i < 8; i++) {
-    for (var j = 0; j < 8; j++) {
         var box = document.createElement("div");
         box.className = "box";
         box.style.backgroundColor = "black";
@@ -38,52 +32,71 @@ for (var i = 0; i < 8; i++) {
         board.appendChild(box);
         boxes.push(box);
     }
+    console.log('created maze.');
 }
-// on click on any boxes add this
 
-for (var i = 0; i < total_rows; i++) {
-    selected_places[i] = [];
-    for (var j = 0; j < total_cols; j++) {
-        selected_places[i][j] = false;
+function making_all_taken_place_false(selected_places, total_rows, total_cols) {
+    console.log('making all taken / selected places false.');
+    for (var i = 0; i < total_rows; i++) {
+        selected_places[i] = [];
+        for (var j = 0; j < total_cols; j++) {
+            selected_places[i][j] = false;
+        }
     }
+    console.log('made all taken / selected places false.');
 }
-console.log(selected_places);
+making_all_taken_place_false(selected_places, total_rows, total_cols);
 
+const initail_setup = () => {
+    console.log('getting data from local storage.');
+    var selected_location_ls = JSON.parse(localStorage.getItem('selected_location'));
+    // if the selected location is not null then update the selected location
+    if (selected_location_ls != null) {
+        selected_location = selected_location_ls;
+        selected_location.forEach((location) => {
+            // get boxes with this row col
+            boxes.forEach((box) => {
+                var row = Number(box.getAttribute('row'));
+                var col = Number(box.getAttribute('col'));
+                console.log(location[0] + "," + location[1]);
+                if (row == location[0] && col == location[1]) {
+                    box.setAttribute('selected', 'true');
+                    box.innerHTML = "👑";
+                }
+            })
+        })
+        updateColorsOfBoxes(selected_location, boxes, selected_places, total_cols, total_rows);
+        turnAllBoxesAsRed(boxes, selected_places);
+    }
+    console.log('got data from local storage.');
+
+}
+initail_setup();
 boxes.forEach((box) => {
+    console.log('adding event listener to box.');
     box.addEventListener("click", (e) => {
+        console.log('box clicked.');
         var row = e.target.getAttribute("row");
         var col = e.target.getAttribute("col");
-        boxes.forEach((box) => {
-            makeBlackWhite(box);
-        })
         if (e.target.getAttribute("selected") == "false") {
-            console.info(
-                "selected place values", selected_places[row][col]
-            )
             if (selected_places[row][col]) {
-                // add class red-pulse-box
                 e.target.classList.add("red-pulse-box");
                 vibrate(400);
                 updateColorsOfBoxes(selected_location, boxes, selected_places, total_cols, total_rows);
                 turnAllBoxesAsRed(boxes, selected_places);
-                vibrate(100);
             } else {
-
                 for (var i = 0; i < total_rows; i++) {
                     selected_places[i] = [];
                     for (var j = 0; j < total_cols; j++) {
-                        console.log("making all selected places false")
                         selected_places[i][j] = false;
                     }
                 }
-                console.log(e.target.getAttribute("selected") + "selecting.");
                 e.target.style.backgroundColor = "black";
                 e.target.setAttribute("selected", "true");
                 selected_location.push([Number(row), Number(col)]);
                 e.target.innerHTML = "👑";
                 updateColorsOfBoxes(selected_location, boxes,
                     selected_places, total_cols, total_rows);
-
                 turnAllBoxesAsRed(boxes, selected_places);
                 vibrate(100);
             }
@@ -92,11 +105,10 @@ boxes.forEach((box) => {
             for (var i = 0; i < total_rows; i++) {
                 selected_places[i] = [];
                 for (var j = 0; j < total_cols; j++) {
-                    console.log("making all selected places false")
                     selected_places[i][j] = false;
                 }
             }
-            console.log(e.target.getAttribute("selected") + "deselecting.");
+
             e.target.setAttribute("selected", "false");
             e.target.innerHTML = "";
             for (var i = 0; i < selected_location.length; i++) {
@@ -110,13 +122,12 @@ boxes.forEach((box) => {
                 selected_places,
                 total_cols,
                 total_rows
-
             );
             turnAllBoxesAsRed(boxes, selected_places);
             vibrate(100);
         }
-        console.log("selected Places")
-        console.log(selected_places);
+        localStorage.setItem("selected_location", JSON.stringify(selected_location));
+        console.log(localStorage.getItem("selected_location"));
         winningMessage(selected_location)
     })
 })
