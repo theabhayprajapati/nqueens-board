4788feb26e96ba40df832af93ac5861b0d9e08ca
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2022-08-29 16:10:08 +0530

    dynamic caching;

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-var CACHE_NAME = 'cache-v11';
-
-// install event pwa
-self.addEventListener('install', function (event) {
-    console.log('Service Worker: Installed');
-}
-);
-// activate event pwa
-self.addEventListener('activate', evt => {
-    //console.log('service worker activated');
-    evt.waitUntil(
-        caches.keys().then(keys => {
-            //console.log(keys);
-            return Promise.all(keys
-                .filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-            );
-        })
-    );
-    console.log('deleted old cache');
-});
-
-
+var CACHE_NAME = 'cache-v13';
+var DYNAMIC_CACHE_NAME = 'dynamic-cache-v13';
 // cache index.html file for offline use
 var urlsToCache = [
     '/',
@@ -30,9 +9,25 @@ var urlsToCache = [
     '/src/index.js',
     'manifest.json',
     '/src/utils/utils.js',
-    '/assets/style/style.css'
+    '/assets/style/style.css',
+    '/offline.html',
 ]
+
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        })
+    })
+}
+
+
+
+// install event pwa
 self.addEventListener('install', function (event) {
+    console.log('Service Worker: Installed');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
@@ -40,17 +35,46 @@ self.addEventListener('install', function (event) {
                 return cache.addAll(urlsToCache);
             })
     );
-}
+});
 
-);
+
+// activate event pwa
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== CACHE_NAME
+                    && key !== DYNAMIC_CACHE_NAME)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+    console.log('deleted old cache');
+});
+
+
+
 self.addEventListener('fetch', function (event) {
     console.log('Service Worker: Fetching')
-    
+
     // show cache  data if available
     event.respondWith(
         caches.match(event.request).then(cache => {
-            return cache || fetch(event.request);
+            return cache || fetch(event.request).then(fetchRes => {
+                return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone());
+                    limitCacheSize(DYNAMIC_CACHE_NAME, 3);
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            if (event.request.url.indexOf('.html') > -1) {
+                return caches.match('/offline.html');
+            }
         })
+
     );
 }
-)
+);
